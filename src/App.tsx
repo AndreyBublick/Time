dcdde98020a0f3dbc692f5dc3cf537e1ef85e408
Andrew <dracon.24041230@mail.ru>
2024-12-29 18:43:28 +0300

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [time, setTime] = React.useState(new Date());
+
+    /*useEffect(() => {
+      setTime(new Date());
+    }, [time]);*/
+
+     useEffect(() => {
+         const timer = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => clearInterval(timer);
+
+    }, [time]);
+
+    return<Wrapper>
+        <TimeStyled >
+            {time.toLocaleTimeString()}
+        </TimeStyled>
+    </Wrapper>
 }
 
 export default App;
+
+
+const TimeStyled = styled.time`
+    color: rgb(151, 151, 151);
+    font-size: 200px;
+    font-weight: 700;
+`;
+const Wrapper = styled.div`
+background-color: #333;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+`;
